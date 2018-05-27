c229d58e2c8d5a5240fdf2881b4caab7e57152b7
Kai Chang <kai0902@hotmail.com>
2018-05-27 18:52:12 +0800

    Init

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='Hello moto')
+    message = TextSendMessage(text='Hello moto yooo')
     line_bot_api.reply_message(
         event.reply_token,
         message)
